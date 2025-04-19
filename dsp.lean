8bce7ffbb7b8021210da608df50932019fb3a15b
DKormann <dominikormann@outlook.com>
2025-04-19 17:02:51 +0200

    kinda

--- dsp.lean
+++ dsp.lean
@@ -20,6 +20,10 @@ inductive Expr : Ty → Type
 | stringlit : String → Expr string
 | lam {a b : Ty} (param : Var a) (body : Expr b) : Expr (arrow a b)
 | app {a b : Ty} (f : Expr (arrow a b)) (x : Expr a) : Expr b
+
+| sup {a:Ty} (label: Nat) (x: Expr a) (y: Expr a) : Expr a
+| dup {a b:Ty} (label: Nat) (x y: Var a) (z: Expr a) (r: Expr b): Expr b
+
 | none : Expr (option a)
 | some  (x : Expr a) : Expr (option a)
 | nil : Expr (list a)
@@ -30,8 +34,6 @@ inductive Expr : Ty → Type
 | lmatch {a b : Ty} (x: Expr $ list a) (n: Expr b) (h: Var a) (t: Var $ list a) (bod: Expr b): Expr b
 | tmatch {a b : Ty} (x: Expr $ tree a) (lfb: Var a) (r: Expr b) (ln rn: Var $ tree a) (bod: Expr b): Expr b
 
-
--- open Expr
 
 class ToMatch (r:Ty) (t: Type) where make :t -> Expr r
 
@@ -63,29 +65,29 @@ def lExpr {a b} [ToExpr a b] (l:List a) : Expr (list b) := match l with
 instance [ToExpr a b] : ToExpr (List a) (Ty.list b) where make o := (lExpr o)
 
 
-
-def g : Expr int := ToExpr.make 22
-def og: Expr (option int) := ToExpr.make (some 22)
-def lg: Expr (list int) := ToExpr.make [22, 33, 44]
-
--- def ll a b := lam a b
-
 def eval  (x:Expr r) := match x with
   | Expr.var v => v.name
   | Expr.intlit n => toString n
   | Expr.stringlit s => s
   | Expr.lam p b => s!"λ{p.name}. {eval b}"
   | Expr.app f x => s!"({eval f} {eval x})"
+
+  | Expr.sup l x y => s!"&{l}\{{eval x} {eval y}}"
+  | Expr.dup l x y z r => s!"!&{l}\{{x.name} {y.name}}={eval z} {eval r}"
+
+  | Expr.nil => "#Nil"
+  | Expr.cons x xs => s!"#Cons\{{eval x} {eval xs}}"
+  | Expr.leaf x => s!"#Leaf\{{eval x}}"
+  | Expr.node l r => s!"#Node\{{eval l} {eval r}}"
+
   | Expr.none => "none"
   | Expr.some x => s!"some {eval x}"
-  | Expr.mmatch i d b x => s!"~({eval i}) \{ #None:{22} #Some\{{b.name}}: {eval x}}"
-  | _ => "not implemented"
-
+  | Expr.mmatch i d b x => s!"~({eval i}) \{ #None:{eval d} #Some\{{b.name}}: {eval x}}"
+  | Expr.lmatch i d h t bod => s!"~({eval i}) \{ #Nil:{eval d} #Cons\{{h.name} {t.name}}: {eval bod}}"
+  | Expr.tmatch x lfb r ln rn bod => s!"~({eval x}) \{ #Leaf\{{lfb.name}:{eval r} #Node\{{ln.name} {rn.name}} : {eval bod}}"
 
 
 notation "(" a "•" b ")" => Expr.app a (ToExpr.make b)
--- notation "λ(" a "→" b ")" => Expr.lam a (ToExpr.make b)
-
 
 def lam {a b} [ToVar a ta] [ToExpr b tb] (x: a) (e: b) : Expr (arrow ta tb) :=
   Expr.lam (ToVar.make x) (ToExpr.make e)
@@ -106,6 +108,9 @@ def as (t:Ty) (e:Expr t) := e
 infixl:56 ":" => as
 infixl:56 "->" => Ty.arrow
 
+
+
+
 #eval
   let x: Var int := ⟨"x"⟩
 
@@ -115,9 +120,7 @@ infixl:56 "->" => Ty.arrow
 
   let lsi :Expr (list int) := (ToExpr.make [22, 33, 44])
 
-  let fn :Expr (arrow int int) := lam x (Expr.var x)
-
-
+  let fn := (int -> int) : lam x (Expr.var x)
 
   let l2 := (int -> int) : lam x x
 
