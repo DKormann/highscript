028c73943fff48d5dc97a7219813fb48b1ada043
DKormann <dominikormann@outlook.com>
2025-04-23 23:59:11 +0200

    refix macro as

--- highscript_solo.lean
+++ highscript_solo.lean
@@ -15,7 +15,6 @@ name: String
 
 inductive Expr : Ty → Type
 | var : (v : Var t) → Expr t
--- | self : (t:Ty) -> Expr t
 | intlit : Nat → Expr int
 | stringlit : String → Expr string
 | lam {a b : Ty} (param : Var a) (body : Expr b) : Expr (arrow a b)
@@ -38,7 +37,9 @@ def comp_term (fn:String) (e: Expr b) : String :=
 
 def collect {t} (e: Expr t) (m: Std.HashMap String String) : (Std.HashMap String String) :=
  match e with
-  | Expr.fn n x => if m.contains n then m else m.insert n $ "@" ++ n ++ " = " ++ comp_term n x
+  | Expr.fn n x =>
+    let m := collect x m
+    if m.contains n then m else m.insert n $ "@" ++ n ++ " = " ++ comp_term n x
   | Expr.lam _ b => collect b m
   | Expr.app f x => collect x (collect f m)
   | Expr.as _ x => collect x m
@@ -64,7 +65,7 @@ instance : ToExpr (Var a) a where make v := Expr.var v
 
 def app {a b : Ty} (f : Expr (arrow a b)) (x : Expr a) : Expr b := Expr.app f x
 
-abbrev fn (n) (e: Expr s) := Expr.fn n e
+abbrev fn (n:String) (e: Expr s) := Expr.fn n e
 
 def astype  (t:Ty) (x: Expr t): Expr t := x
 
@@ -82,23 +83,26 @@ def tag (n:String) : Expr s := Expr.ftag n s
 
 macro:100 "lam" x:ident "->" body:term:100 : term => `(makelam fun $x => $body)
 
-macro:100 "@" n:ident "=" val:term "; " body:term : term=> `(let $n := $val; $body)
-macro:50 v:term:50 "as" t:term:51 : term => `(astype $t $v)
 
+macro:50 "@" n:ident ":" typ:term:50 "; " body:term:50 : term=> `(let $n := Expr.ftag $(Lean.quote (n.getId.toString)) $typ; $body)
+macro:50 "@" n:ident "=" val:term:50 "; " body:term:50 : term=> `(let $n := fn $(Lean.quote (n.getId.toString)) $val; $body)
+macro:50 v:term:50 "as" t:term:51 : term => `(astype $t $v)
+macro:50  a:term:50 "(" b:term:50 ")" : term => `(Expr.app $a $b)
 
 #eval
-
-
-
   let x := Expr.intlit 44 as int
   let f := lam x -> (x) as (int -> int)
-
-
-
-  compile f
-
-
+  let main := fn "main" $ lam x -> (x) as (int -> int)
+  compile main
 
 #eval
-  @tt = 44;
-  tt
+
+  @tt : int -> int;
+  @tt = lam x -> (tt (x)) as (int -> int);
+
+  let d := Expr.intlit 22 as int;
+
+
+  @main = tt;
+
+  compile main
