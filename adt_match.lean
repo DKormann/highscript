e362b33f6e97e90033478691ff9edfdd79ad3519
DKormann <dominikormann@outlook.com>
2025-05-02 15:49:05 +0200

    nicer linting

--- adt_match.lean
+++ adt_match.lean
@@ -7,7 +7,7 @@ deriving DecidableEq
 abbrev TT := Tvar.T
 abbrev RR := Tvar.Rec
 
-@[reducible]
+
 def Tvar.choose {a} (t:a) (r:a) : Tvar → a | Tvar.T => t | Tvar.Rec => r
 
 def x  := Tvar.T.choose 1 2
@@ -48,13 +48,17 @@ mutual
 
   inductive VariantInst : (a: Adt) -> (t:Ty) -> (v: List Tvar) -> Type
     | nil : VariantInst a t []
-    | cons : (tv:Tvar) -> (x: (Expr $ tv.choose t $ a[t] )) -> (rest: VariantInst a t xs) -> VariantInst a t (tv::xs)
+    | T: (x: Expr t) -> (rest: VariantInst a t xs) -> VariantInst a t (Tvar.T::xs)
+    | R : (x: (Expr $ a[t])) -> (rest: VariantInst a t xs) -> VariantInst a t (Tvar.Rec::xs)
+    -- | cons : (tv:Tvar) -> (x: (Expr $ tv.choose t $ a[t] )) -> (rest: VariantInst a t xs) -> VariantInst a t (tv::xs)
 
   inductive DataInst : (a : Adt) → (t:Ty) -> Type
     | k : (n:Fin a.Variants.length ) -> (data : VariantInst a t a.Variants[n]) -> DataInst a t
 
   inductive MatchCase : (r:Ty) -> (t:Ty) -> (v: List Tvar) -> (res: Ty) -> Type
     | nil : (e:Expr res) -> MatchCase r t [] res
+    | T : (x: Var t) -> (rest: MatchCase r t xs res) -> MatchCase r t (Tvar.T::xs) res
+    | R : (x: (Var r)) -> (rest: MatchCase r t xs res) -> MatchCase r t (Tvar.Rec::xs) res
     | cons : (tv.choose (Var t) (Var r)) -> (MatchCase r t xs res) -> MatchCase r t (tv::xs) res
 
   inductive Match : (a:Adt) -> (vs:List (List Tvar)) -> (t:Ty) -> (res:Ty) -> Type
@@ -71,8 +75,8 @@ def Ctr : (a:Adt) -> (t:Ty) -> (var: List Tvar) -> Type
 
 def ctr : (a:Adt) -> (t:Ty) -> (v: List Tvar) -> ((VariantInst a t v) -> Expr (a[t])) -> (Ctr a t v)
   | _, _, [], f => (f VariantInst.nil)
-  | a, t, Tvar.T::xs, f => fun x => ctr a t xs fun v => f $ VariantInst.cons Tvar.T x v
-  | a, t, Tvar.Rec::xs, f => fun x => ctr a t xs fun v => f $ VariantInst.cons Tvar.Rec x v
+  | a, t, Tvar.T::xs, f => fun x => ctr a t xs fun v => f $ VariantInst.T  x v
+  | a, t, Tvar.Rec::xs, f => fun x => ctr a t xs fun v => f $ VariantInst.R x v
 
 def mkctr (a:Adt) (t) (n:Nat) (p:n<a.Variants.length := by decide): Ctr a t a.Variants[n] :=
   ctr a t a.Variants[n] fun v => Expr.data $ DataInst.k ⟨n,p⟩ v
@@ -84,13 +88,18 @@ def makelam (tag:String) (builder : (Expr a) -> Expr b) : Expr (arrow a b) :=
 
 macro:100 "lam" x:ident "->" body:term:100 : term => `(makelam $(Lean.quote (x.getId.toString)) fun $x => $body)
 
+
+
 def CaseMaker (a t res: Ty) (vars : List Tvar) : (v:List Tvar) -> Type
   | [] => (Expr res) -> (MatchCase a t vars res)
-  | tv::vs => (tv.choose (Var t) (Var a)) -> (CaseMaker a t res vars vs)
+  | Tvar.T::vs => (Var t) -> (CaseMaker a t res vars vs)
+  | Tvar.Rec::vs => (Var a) -> (CaseMaker a t res vars vs)
+
 
 def caseMaker (a t res: Ty) (vars: List Tvar) : (v: List Tvar) -> (MatchCase a t v res -> MatchCase a t vars res) -> (CaseMaker a t res vars v)
   | [], f => fun (ex:Expr res) => f (MatchCase.nil ex)
-  | tv::vs, f => fun (va: tv.choose (Var t) (Var (a))) => caseMaker a t res vars vs fun (cs) => f (MatchCase.cons va cs)
+  | Tvar.T::vs, f => fun (va: Var t) => caseMaker a t res vars vs fun (cs) => f (MatchCase.T va cs)
+  | Tvar.Rec::vs, f => fun (va: Var a) => caseMaker a t res vars vs fun (cs) => f (MatchCase.R va cs)
 
 def mkcase  (a:Adt) (t res:Ty) (n:Nat) (p: n<a.Variants.length:= by decide) (vs:List Tvar := a.Variants[n]): (CaseMaker (a[t]) t res vs vs):=
   caseMaker (a[t]) t res vs vs (fun x => x)
@@ -119,8 +128,9 @@ def cm : CaseMaker (list[int]) int int cons cons := mkcase list int int 1
 def ccf : (Var int) -> (Var (list[int])) -> Expr int -> MatchCase (list[int]) int cons int := cm
 def ccs := cm (newVar "h") (newVar "t") (Expr.intlit 2)
 
+
 def matcc := (mkmatch list int int)
   ((mkcase list int int 0) (Expr.intlit 1))
-  ((mkcase list int int 1) (newVar "h" : Var int) (newVar "t") (Expr.intlit 2))
+  ((mkcase list int int 1) (newVar "h" : Var (int)) (newVar "t") (Expr.intlit 2))
 
 def matcv : Expr int := Expr.matcher lsi matcc
